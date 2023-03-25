11e120c84a9f20464a49a418e03f8ab35b8cd789
Degen Munsy <munner7@gmail.com>
2023-03-25 06:46:24 -0400

    tests added. test-ledger, target removed to git push

--- programs/calculator/src/lib.rs
+++ programs/calculator/src/lib.rs
@@ -1,48 +1,120 @@
+// Import the anchor_lang prelude, which contains many commonly used items
 use anchor_lang::prelude::*;
+
+// Import the solana_program entrypoint module to handle program results
 use anchor_lang::solana_program::entrypoint::ProgramResult;
 
-declare_id!("2VsDgBxxmv6cyKAJjjmKHggt777rN4ELvQ2W6yCzzpqp");
+// Declare the program ID, which is the unique identifier for your program on the Solana blockchain
+declare_id!("fRprMGtWFJ9oGAjCedCc9xSfZoQfWZm43Z2pp29z5ML");
 
+// Define the calculator module
 #[program]
 pub mod calculator {
+    // Import the items from the parent scope
     use super::*;
 
+    // Define the create function, which initializes the calculator account with a greeting
     pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
+        // Get a mutable reference to the calculator account
         let calculator = &mut ctx.accounts.calculator;
+
+        // Set the calculator account's greeting field to the input message
         calculator.greeting = init_message;
+
+        // Return success
         Ok({})
     }
 
+    // Define the add function, which calculates the sum of two numbers
     pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) -> ProgramResult {
+        // Get a mutable reference to the calculator account
         let calculator = &mut ctx.accounts.calculator;
+
+        // Set the calculator account's result field to the sum of the input numbers
         calculator.result = num1 + num2;
+
+        // Return success
+        Ok(())
+    }
+
+    // Define the subtract function, which calculates the difference of two numbers
+    pub fn subtract(ctx: Context<Subtraction>, num1: i64, num2: i64) -> ProgramResult {
+         let calculator = &mut ctx.accounts.calculator;
+         calculator.result = num1 - num2;
+         Ok(())
+    }
+    
+        // Define the multiply function, which calculates the product of two numbers
+    pub fn multiply(ctx: Context<Multiplication>, num1: i64, num2: i64) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 * num2;
         Ok(())
     }
     
+        // Define the divide function, which calculates the quotient of two numbers
+    pub fn divide(ctx: Context<Division>, num1: i64, num2: i64) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 / num2;
+        Ok(())
+    }
 }
 
+// Define the account structs used in the create function
 #[derive(Accounts)]
 pub struct Create<'info> {
-
+    // Define the calculator account, which is initialized, paid for by the user, and has a size of 264 bytes
     #[account(init, payer=user, space=264)]
     pub calculator: Account<'info, Calculator>,
 
+    // Define the user account, which is mutable and a signer
     #[account(mut)]
     pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
 
+    // Define the system_program account, which represents the Solana system program
+    pub system_program: Program<'info, System>,
 }
 
+// Define the account structs used in the add function
 #[derive(Accounts)]
 pub struct Addition<'info> {
+    // Define the calculator account, which is mutable
     #[account(mut)]
     pub calculator: Account<'info, Calculator>,
 }
 
+// Define the account structs used in the subtract function
+#[derive(Accounts)]
+pub struct Subtraction<'info> {
+    // Define the calculator account, which is mutable
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+
+// Define the account structs used in the multiply function
+#[derive(Accounts)]
+pub struct Multiplication<'info> {
+    // Define the calculator account, which is mutable
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+
+// Define the account structs used in the divide function
+#[derive(Accounts)]
+pub struct Division<'info> {
+    // Define the calculator account, which is mutable
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+
+// Define the Calculator account data structure
 #[account]
 pub struct Calculator {
+    // Declare a greeting field of type String
     greeting: String,
+
+    // Declare a result field of type i64 (64-bit signed integer)
     result: i64
 }
+
 
 
--- tests/calculator.ts
+++ tests/calculator.ts
@@ -1,48 +1,97 @@
+// Import the required modules from the anchor library
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Calculator } from "../target/types/calculator";
-const { SystemProgram } = anchor.web3
-import { expect } from 'chai';
+const { SystemProgram } = anchor.web3;
+import { expect } from "chai";
 
-//Moka works using predescribed it blocks 
+// Describe a test suite for the calculator program
 describe("calculator", () => {
-  // Configure the client to use the local cluster.
+  // Set the anchor provider to the local environment
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  //Referencing the program - Abstraction that allows us to call methods of our SOL program.
+  // Create a reference to the calculator program
   const program = anchor.workspace.Calculator as Program<Calculator>;
   const programProvider = program.provider as anchor.AnchorProvider;
 
-  //Generating a keypair for our Calculator account
+  // Generate a key pair for the calculator account
   const calculatorPair = anchor.web3.Keypair.generate();
 
-  const text = "Summer School Of Solana"
+  const text = "Summer School Of Solana";
 
-  //Creating a test block
+  // Define a test case for creating a calculator instance
   it("Creating Calculator Instance", async () => {
-    //Calling create instance - Set our calculator keypair as a signer
-    await program.methods.create(text).accounts(
-      {
-          calculator: calculatorPair.publicKey,
-          user: programProvider.wallet.publicKey,
-          systemProgram: SystemProgram.programId,
-      }
-  ).signers([calculatorPair]).rpc()
+    // Call the create method from the calculator program
+    await program.methods
+      .create(text)
+      .accounts({
+        calculator: calculatorPair.publicKey,
+        user: programProvider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([calculatorPair])
+      .rpc();
 
-  //We fecth the account and read if the string is actually in the account
-  const account = await program.account.calculator.fetch(calculatorPair.publicKey)
-  expect(account.greeting).to.eql(text)
+    // Fetch the calculator account and check if the greeting field is set correctly
+    const account = await program.account.calculator.fetch(calculatorPair.publicKey);
+    expect(account.greeting).to.eql(text);
   });
 
-  //Another test step - test out addition
-  it('Addition',async () => {
-    await program.methods.add(new anchor.BN(2), new anchor.BN(3))
-    .accounts({
+  // Define a test case for addition
+  it("Addition", async () => {
+    await program.methods
+      .add(new anchor.BN(2), new anchor.BN(3))
+      .accounts({
         calculator: calculatorPair.publicKey,
-    })
-    .rpc()
-    const account = await program.account.calculator.fetch(calculatorPair.publicKey)
-    expect(account.result).to.eql(new anchor.BN(5))
-})
+      })
+      .rpc();
+
+    // Fetch the calculator account and check if the result field is set correctly
+    const account = await program.account.calculator.fetch(calculatorPair.publicKey);
+    expect(account.result).to.eql(new anchor.BN(5));
+  });
+
+
+ // Define a test case for subtraction
+ it("Subtraction", async () => {
+   await program.methods
+     .subtract(new anchor.BN(7), new anchor.BN(4))
+     .accounts({
+       calculator: calculatorPair.publicKey,
+      })
+      .rpc();
+
+    // Fetch the calculator account and check if the result field is set correctly
+    const account = await program.account.calculator.fetch(calculatorPair.publicKey);
+    expect(account.result).to.eql(new anchor.BN(3));
+  });
+
+ // Define a test case for multiplication
+ it("Multiplication", async () => {
+   await program.methods
+     .multiply(new anchor.BN(3), new anchor.BN(4))
+     .accounts({
+       calculator: calculatorPair.publicKey,
+      })
+      .rpc();
+
+    // Fetch the calculator account and check if the result field is set correctly
+    const account = await program.account.calculator.fetch(calculatorPair.publicKey);
+    expect(account.result).to.eql(new anchor.BN(12));
+  });
+
+ // Define a test case for division
+ it("Division", async () => {
+   await program.methods
+     .divide(new anchor.BN(12), new anchor.BN(4))
+     .accounts({
+       calculator: calculatorPair.publicKey,
+      })
+      .rpc();
+
+    // Fetch the calculator account and check if the result field is set correctly
+    const account = await program.account.calculator.fetch(calculatorPair.publicKey);
+    expect(account.result).to.eql(new anchor.BN(3));
+  });
 
 });
